7b32d8507946af4a8f6a412ea056aa0dee8a4824
kung <lwkung@iii.org.tw>
2019-07-11 22:55:26 +0800

    login & Email verify

--- app/src/main/java/com/test/firebaseshop/MainActivity.kt
+++ app/src/main/java/com/test/firebaseshop/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.test.firebaseshop
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
+
+    private val RC_SIGNIN = 100
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,36 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        verify_email.setOnClickListener {
+            FirebaseAuth.getInstance().currentUser?.sendEmailVerification()
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Snackbar.make(it, "Verify email sent", Snackbar.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+
+    override fun onAuthStateChanged(p0: FirebaseAuth) {
+        val user = FirebaseAuth.getInstance().currentUser
+        Log.d(TAG, "onAuthStateChanged: ${user?.uid}")
+        if (user != null) {
+            user_info.setText("Email: ${user.email} / ${user.isEmailVerified}")
+            verify_email.visibility = if (user.isEmailVerified) View.GONE else View.VISIBLE
+        } else {
+            user_info.setText("Not login")
+            verify_email.visibility = View.GONE
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        FirebaseAuth.getInstance().removeAuthStateListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,6 +72,17 @@ class MainActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
+            R.id.action_signin -> {
+                startActivityForResult(
+                    Intent(this, SignInActivity::class.java),
+                    RC_SIGNIN
+                )
+                true
+            }
+            R.id.action_signout -> {
+                FirebaseAuth.getInstance().signOut()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
---
+++ app/src/main/java/com/test/firebaseshop/SignInActivity.kt
@@ -0,0 +1,68 @@
+package com.test.firebaseshop
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        signUp.setOnClickListener { view ->
+            signUp()
+        }
+        login.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(
+                email.text.toString(),
+                password.text.toString()
+            )
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    setResult(Activity.RESULT_OK)
+                    finish()
+                } else {
+                    AlertDialog.Builder(this@SignInActivity)
+                        .setTitle("Login In")
+                        .setMessage(task.exception?.message)
+                        .setPositiveButton("OK", null)
+                        .show()
+                }
+            }
+    }
+
+    private fun signUp() {
+        val sEmail = email.text.toString()
+        val sPasswd = password.text.toString()
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(sEmail, sPasswd)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    AlertDialog.Builder(this@SignInActivity)
+                        .setTitle("Sign Up")
+                        .setMessage("Account created")
+                        .setPositiveButton("OK") { dialog, which ->
+                            setResult(Activity.RESULT_OK)
+                            finish()
+                        }.show()
+
+                } else {
+                    AlertDialog.Builder(this@SignInActivity)
+                        .setTitle("Sign Up")
+                        .setMessage(task.exception?.message)
+                        .setPositiveButton("OK", null)
+                        .show()
+                }
+            }
+    }
+}

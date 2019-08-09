51df14d52157bbf93ce9fca8ae98fc37034a1b01
kung <lwkung5280@gmail.com>
2019-08-09 16:46:02 +0800

    收藏商品清單功能設計與開發 & 解決選取商品無動作之bug(firebase database補上category欄位)

--- app/src/main/java/com/test/firebaseshop/DetailActivity.kt
+++ app/src/main/java/com/test/firebaseshop/DetailActivity.kt
@@ -3,8 +3,10 @@ package com.test.firebaseshop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.test.firebaseshop.model.Item
+import com.test.firebaseshop.model.WatchItem
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
@@ -21,6 +23,39 @@ class DetailActivity : AppCompatActivity() {
         web.settings.javaScriptEnabled = true
         web.loadUrl("https://litotom.com/shop/android9/")
 //        web.loadUrl(item.content)
+
+        // read watch item
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+        FirebaseFirestore.getInstance().collection("users")
+            .document(uid!!)
+            .collection("watchItems")
+            .document(item.id).get()
+            .addOnCompleteListener { tack ->
+                if (tack.isSuccessful) {
+                    val watchItem = tack.result?.toObject(WatchItem::class.java)
+                    if (watchItem != null) {
+                        watch.isChecked = true
+                    }
+                }
+            }
+        // watches
+        watch.setOnCheckedChangeListener { buttonView, isChecked ->
+            val uid = FirebaseAuth.getInstance().currentUser?.uid
+            if (isChecked) {
+                FirebaseFirestore.getInstance().collection("users")
+                    .document(uid!!)
+                    .collection("watchItems")
+                    .document(item.id)
+                    .set(WatchItem(item.id))
+            } else {
+                FirebaseFirestore.getInstance().collection("users")
+                    .document(uid!!)
+                    .collection("watchItems")
+                    .document(item.id)
+                    .delete()
+            }
+
+        }
     }
 
     override fun onStart() {
